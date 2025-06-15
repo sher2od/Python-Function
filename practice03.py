3dd9475d56d5fc8c6243954c380bb386a5314e85
sher2od <sherzodqarshiboyev543@gmail.com>
2025-06-15 10:08:56 +0300

    l3

--- practice03.py
+++ practice03.py
@@ -1,10 +1,20 @@
-def aniqlovchi(num):
-   if num % 2 == 0:
-      print(True)
-   else:
-      print(False)
+def is_even(number: float) -> bool:
+    """
+    Son juftligini tekshiradi.
 
+    Args:
+        number (float): Tekshiriladigan son.
 
-number = int(input("son >> "))
+    Returns:
+        bool: Agar juft bolsa True, aks holda False.
+    """
+    return number % 2 == 0
 
-aniqlovchi(number)
+def print_even_message():
+    number = float(input("Sonni kiriting >> "))
+    if is_even(number):
+        print("Bu son JUFT.")
+    else:
+        print("Bu son TOQ.")
+
+print_even_message()

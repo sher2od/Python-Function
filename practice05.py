a434dd286e7399da874309c65db3f22b1ed15700
sher2od <sherzodqarshiboyev543@gmail.com>
2025-06-15 10:23:03 +0300

    l5

--- practice05.py
+++ practice05.py
@@ -1,12 +1,28 @@
 from random import randint
 
-password = randint(1,10)
+password = randint(1, 10)
 
-def sirli_son(n):
-    if n == password:
-        print("Topdingiz")
+def check_guess(secret: int, guess: int) -> bool:
+    """
+    Foydalanuvchi taxmini togriligini tekshiradi.
+
+    Args:
+        secret (int): Kompyuter tanlagan sirli son.
+        guess (int): Foydalanuvchi kiritgan son.
+
+    Returns:
+        bool: True agar togri topsa, False aks holda.
+    """
+    return secret == guess
+
+def print_result():
+    guess = int(input("1 dan 10 gacha son kiriting: "))
+    if check_guess(password, guess):
+        print("Topdingiz!")
     else:
-        print("Toplomadingiz")
+        print(f"Topolmadingiz! Togri javob: {password}")
 
-number = int(input("Butun son kiriting "))
-sirli_son(number)
+print_result()
+
+
+

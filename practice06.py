0b1c9dcb652fb9ddc2f75a74b2a4202eca0bc3be
sher2od <sherzodqarshiboyev543@gmail.com>
2025-06-15 10:24:25 +0300

    l6

--- practice06.py
+++ practice06.py
@@ -1,8 +1,18 @@
-def phone_number(num):
+def phone_number(num: str) -> None:
+    """
+    Telefon raqami uzunligini tekshiradi (raqam 9 belgidan iborat bo‘lishi kerak).
+
+    Args:
+        num (str): Foydalanuvchi tomonidan kiritilgan telefon raqami (raqamlar matn ko‘rinishida).
+
+    Returns:
+        None: Natijani ekranga chiqaradi (True yoki False).
+    """
     if len(num) == 9:
         print(True)
     else:
         print(False)
 
-number = input("Telfon raqamiz ")
+
+number = input("Telefon raqamingizni kiriting: ")
 phone_number(number)

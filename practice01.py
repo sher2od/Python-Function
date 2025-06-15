9eb76ee3ab38bb7598a3afaad13eb8af0bdf2e6f
sher2od <sherzodqarshiboyev543@gmail.com>
2025-06-15 09:44:21 +0300

    l1

--- practice01.py
+++ practice01.py
@@ -1,18 +1,76 @@
 def add(a,b):
     result = a + b
     return result
+    return result
+'''Ikki sonni yigindisi
+
+    Args:
+    a(float): birinchi son
+    b(float): ikkinchi son
+
+    Returns:
+    float:yigindi
+
+    Raises:
+    TypeError:type error
+
+    '''
 
 def subtract(a,b):
     result = a - b
     return result
 
+    return result
+    '''Ikki sonni ayirmasi
+
+    Args:
+    a(float): birinchi son
+    b(float): ikkinchi son
+
+    Returns:
+    float:ayirmasi
+
+    Raises:
+    TypeError:type error
+
+    '''
+
 def multiply(a,b):
     result = a * b
     return result
 
+    '''Ikki sonni kpaytmasi
+
+    Args:
+    a(float): birinchi son
+    b(float): ikkinchi son
+
+    Returns:
+    float:kopaytmasi
+
+    Raises:
+    TypeError:type error
+
+    '''
+
+
 def divid(a,b):
     result = a / b
     return result
+
+    '''Ikki sonni bolim=nmasi
+
+    Args:
+    a(float): birinchi son
+    b(float): ikkinchi son
+
+    Returns:
+    float:bolinmasi
+
+    Raises:
+    TypeError:type error
+
+    '''
  
 def main():
     a = int(input("a >> "))
